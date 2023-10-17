f2e47ffafef81b8f601ba130296aa26c3d5819d5
vartika4160 <139868043+vartika4160@users.noreply.github.com>
2023-10-18 00:35:35 +0530

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// Function to calculate factorial recursively
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int num;
+    cout << "Enter a non-negative integer: ";
+    cin >> num;
+    
+    // Check if the input is non-negative
+    if (num < 0) {
+        cout << "Factorial is not defined for negative numbers." << endl;
+    } else {
+        int result = factorial(num);
+        cout << "Factorial of " << num << " is " << result << endl;
+    }
+    
+    return 0;
+}
