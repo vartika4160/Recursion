d3549731d95630ebc55965adda1491be8820e379
vartika4160 <139868043+vartika4160@users.noreply.github.com>
2023-10-18 00:36:50 +0530

    Create sum_1_to_n.cpp

---
+++ sum_1_to_n.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+// Recursive function to calculate the sum of integers from 1 to 'n'
+int sumOfIntegers(int n) {
+    // Base case: If 'n' is 1, return 1
+    if (n == 1) {
+        return 1;
+    }
+    // Recursive case: Calculate the sum from 1 to 'n' by adding 'n' and the sum of integers from 1 to 'n-1'
+    return n + sumOfIntegers(n - 1);
+}
+
+int main() {
+    int n;
+    cout << "Enter a positive integer 'n': ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Please enter a positive integer." << endl;
+        return 1; // Exit with an error code
+    }
+
+    int result = sumOfIntegers(n);
+    cout << "The sum of integers from 1 to " << n << " is: " << result << endl;
+
+    return 0;
+}
